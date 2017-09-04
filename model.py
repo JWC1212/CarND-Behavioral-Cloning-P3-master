7a9e225a91cafd1fc3977015d7a66a2e891e9cc5
JWC1212 <jonathenwu@hotmail.com>
2017-09-04 23:02:14 +0800

    Update model.py

--- model.py
+++ model.py
@@ -53,19 +53,23 @@ validate_generator = sample_generator(validation_samples, 128)
 from keras.models import Sequential
 from keras.layers.core import Flatten, Dense ,Lambda
 from keras.layers import Cropping2D
-from keras.layers.convolutional import Conv2D
-#from keras.layers.pooling import MaxPool2D
+from keras.layers.convolutional import Convolution2D
+#from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: (x/255.0) - 0.5, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
-model.add(Conv2D(6, (5, 5),strides=(1, 1), padding='valid',activation='relu'))
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
 #model.add(MaxPool2D(pool_size=(3,3),strides=(2,2)))
-model.add(Conv2D(6, (5, 5),strides=(1, 1), padding='valid',activation='relu'))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
 #model.add(MaxPool2D(pool_size=(3,3),strides=(2,2)))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse',optimizer='Adam',metrics=['accuracy'])
