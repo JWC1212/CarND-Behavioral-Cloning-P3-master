d8690c6b42b5d0b8df47a01831a4e2f9037cc447
JWC1212 <jonathenwu@hotmail.com>
2017-09-04 23:08:16 +0800

    Update model.py

--- model.py
+++ model.py
@@ -51,7 +51,7 @@ train_generator = sample_generator(train_samples, 128)
 validate_generator = sample_generator(validation_samples, 128)
     
 from keras.models import Sequential
-from keras.layers.core import Flatten, Dense ,Lambda
+from keras.layers import Flatten, Dense ,Lambda
 from keras.layers import Cropping2D
 from keras.layers.convolutional import Convolution2D
 #from keras.layers.pooling import MaxPooling2D
@@ -59,11 +59,11 @@ from keras.layers.convolutional import Convolution2D
 model = Sequential()
 model.add(Lambda(lambda x: (x/255.0) - 0.5, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
-model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(24, 5, 5, activation='relu'))
 #model.add(MaxPool2D(pool_size=(3,3),strides=(2,2)))
-model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(36, 5, 5, activation='relu'))
 #model.add(MaxPool2D(pool_size=(3,3),strides=(2,2)))
-model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(48, 5, 5, activation='relu'))
 model.add(Convolution2D(64, 3, 3, activation='relu'))
 
 model.add(Flatten())
